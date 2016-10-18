3b95b2d3863071b8bf337d332e60fb2700d2f3d9
Soringaming <thehawksquad50@gmail.com>
2016-10-18 17:39:35 +1100

    Started the "StartCountdown" class

--- src/com/MagePVP/Threads/StartCountdown.java
+++ src/com/MagePVP/Threads/StartCountdown.java
@@ -4,6 +4,8 @@
 
 package com.MagePVP.Threads;
 
+import org.bukkit.Bukkit;
+
 /**
  * Created by Soringaming on 10/18/2016.
  */
@@ -15,7 +17,23 @@ public class StartCountdown implements Runnable {
     public void run() {
 
         timeUntilStart = 60;
-        while(true) {
+        while (true) {
+            for (; timeUntilStart >= 0; timeUntilStart--) {
+                if (timeUntilStart == 0) {
+                    //Start le game
+                    break;
+                }
+                if (timeUntilStart % 10 == 0 || timeUntilStart < 10) {
+                    // say some shit
+                }
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Bukkit.shutdown();
+                }
+            }
 
         }
 

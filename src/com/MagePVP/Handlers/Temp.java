feec24aadb0e13fd4404d53f43d3b95fd4f343c1
Soringaming <thehawksquad50@gmail.com>
2016-10-18 17:13:44 +1100

    Added temp classes to get them sneaky packages in

---
+++ src/com/MagePVP/Handlers/Temp.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2016. Soringaming and Sobki. All rights reserved.
+ */
+
+package com.MagePVP.Handlers;
+
+/**
+ * Created by Soringaming on 10/18/2016.
+ */
+public class Temp {
+}

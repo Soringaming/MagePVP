76aa8b286f0a67f61b16c39e9e49bbf15f450f1e
Soringaming <thehawksquad50@gmail.com>
2016-10-18 16:45:57 +1100

    Started main project stuffs

---
+++ src/com/MagePVP/MagePVP.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2016. Soringaming and Sobki. All rights reserved.
+ */
+
+package com.MagePVP;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Soringaming on 10/18/2016.
+ */
+public class MagePVP extends JavaPlugin {
+}
---
+++ src/com/MagePVP/Threads/StartCountdown.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2016. Soringaming and Sobki. All rights reserved.
+ */
+
+package com.MagePVP.Threads;
+
+/**
+ * Created by Soringaming on 10/18/2016.
+ */
+public class StartCountdown implements Runnable {
+
+    private static int timeUntilStart;
+
+    @Override
+    public void run() {
+
+        timeUntilStart = 60;
+        while(true) {
+
+        }
+
+    }
+
+}
